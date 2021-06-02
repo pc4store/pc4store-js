0c9f22afee1ce06fc3af801987c19b4029508244
Evgen <e.roginskiy@list.family>
2021-06-02 17:44:20 +0300

    - rewrote all from functional to class paradigm - added new methods: `createTransfer`, `getTransferInfo`

---
+++ src/client.ts
@@ -0,0 +1,111 @@
+import * as crypto from 'crypto';
+import fetch from 'node-fetch';
+import * as ed from 'noble-ed25519';
+import {
+  SuccessfulOrderResponse,
+  SuccessfulTransferResponse,
+  CreatePaymentLinkParams,
+  CreateTransferParams,
+  CreateTransferOutput,
+  CreatePaymentLinkOutput,
+  GetOrderOutput,
+  GetTransferOutput,
+} from './types';
+
+const HOST = 'https://api.pc4.store/v1';
+const PUBLIC_KEY = '69f72437e2e359a3e5c29fe9a7e0d509345cc57b7bfca0b470598d679a349806';
+
+export class PC4StoreClient {
+  storeId: string;
+  secretKey: string;
+
+  constructor({ storeId, secretKey }: { storeId: string; secretKey: string }) {
+    this.storeId = storeId;
+    this.secretKey = secretKey;
+  }
+
+  constructHeaders() {
+    return {
+      'Content-Type': 'application/json',
+      Authorization: 'Basic ' + Buffer.from(this.storeId + ':' + this.secretKey).toString('base64'),
+    };
+  }
+
+  /**
+   * Verify response
+   * @param signature - hex string, taken from header
+   * @param data - callback's body
+   */
+  async verify(
+    headers: { [key: string]: unknown; signature: string },
+    data: SuccessfulOrderResponse | SuccessfulTransferResponse,
+  ) {
+    return await ed.verify(
+      headers.signature,
+      crypto.createHash('sha256').update(JSON.stringify(data)).digest('hex'),
+      PUBLIC_KEY,
+    );
+  }
+
+  /**
+   * @param paymentParams - all order params
+   * @returns payment link for client
+   */
+  async createOrder(paymentParams: CreatePaymentLinkParams): Promise<CreatePaymentLinkOutput> {
+    return await fetch(`${HOST}/create`, {
+      method: 'POST',
+      headers: this.constructHeaders(),
+      body: JSON.stringify(paymentParams),
+    }).then((j) => j.json());
+  }
+  /**
+   * Get order's data by orderId. Verify built in
+   * @param orderId - order's id in payment system
+   * @returns order
+   */
+  async getOrder(orderId: string): Promise<GetOrderOutput> {
+    const response = await fetch(`${HOST}/order_info/${orderId}`, {
+      method: 'GET',
+      headers: this.constructHeaders(),
+    });
+
+    const data = await response.json();
+    const isValidSignature = await this.verify({ signature: response.headers.get('signature') || '' }, data);
+
+    if (!isValidSignature) throw Error('Invalid signature');
+
+    return data;
+  }
+
+  /**
+   * Create transfer
+   * @param transferParams - all transfer params
+   * @returns transfer's details
+   */
+  async createTransfer(transferParams: CreateTransferParams): Promise<CreateTransferOutput> {
+    return await fetch(`${HOST}/transfer`, {
+      method: 'POST',
+      headers: this.constructHeaders(),
+      body: JSON.stringify(transferParams),
+    }).then((j) => j.json());
+  }
+
+  /**
+   * Get transfer's details by transfer's id. Verify - built in
+   * @param transferId - transfers's id in payment system
+   * @returns transfer's details
+   */
+  async getTransfer(transfer: string): Promise<GetTransferOutput> {
+    const response = await fetch(`${HOST}/transfer_info/${transfer}`, {
+      method: 'GET',
+      headers: this.constructHeaders(),
+    });
+
+    const data = await response.json();
+    const isValidSignature = await this.verify({ signature: response.headers.get('signature') || '' }, data);
+
+    if (!isValidSignature) throw Error('Invalid signature');
+
+    return data;
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,95 @@
+export type CreatePaymentLinkParams = {
+  currency_name: Currency['name'];
+  currency_smart_contract: Currency['smart_contract'];
+  amount_to_pay: string;
+  response_url: string;
+  expiration_time?: number;
+  merchant_order_id?: string;
+  description?: string;
+  success_payment_redirect_url?: string;
+  failed_payment_redirect_url?: string;
+};
+
+export type CreateTransferParams = {
+  amount: string;
+  currency_name: Currency['name'];
+  currency_smart_contract: Currency['smart_contract'];
+  eos_account: string;
+  response_url?: string;
+};
+
+export type Currency = {
+  name: 'USDCASH' | 'UAHCASH' | 'RUBCASH';
+  smart_contract: string;
+  precission: number;
+};
+
+export type AmountType = {
+  full_amount: string;
+  amount_after_tax: string;
+  fee: string;
+};
+
+export type Transfer = {
+  id: string;
+  amount: AmountType;
+  currency: Currency;
+  sender: string;
+  receiver: string;
+  txn_type: 'ORDER_PAYMENT' | 'PAYBACK' | 'DEPOSIT' | 'WITHDRAW';
+  status: 'INITIATED' | 'SENDED' | 'RECEIVED' | 'ACCEPTED' | 'DECLINED' | 'FAILED';
+  memo: string;
+  action: {
+    txid: string;
+    block_number: number;
+    global_sequence: number;
+    is_irreversible: boolean;
+    is_reversed: boolean;
+  };
+};
+
+export type Order = {
+  id: string;
+  sequent_number: number;
+  amount: AmountType;
+  currency: Currency;
+  status: 'PAID' | 'CREATED' | 'MONEY_RECEIVED' | 'CANCELLED' | 'EXPIRED';
+  expiration_date: string;
+  payment_url: string;
+  response_url: string;
+  success_payment_redirect_url?: string;
+  failed_payment_redirect_url?: string;
+  is_test: boolean;
+  details: {
+    merchant_order_id?: string;
+    description?: string;
+  };
+  payment_transfer?: Transfer | null;
+};
+
+export type SuccessfulOrderResponse = {
+  status: 'OK';
+  payload: {
+    order: Order;
+  };
+};
+
+export type SuccessfulTransferResponse = {
+  status: 'OK';
+  payload: {
+    transfer: Transfer;
+  };
+};
+
+export type FailureResponse = {
+  status: 'ERROR';
+  error: string;
+};
+
+export type CallbackResponse = SuccessfulOrderResponse;
+
+export type CreatePaymentLinkOutput = SuccessfulOrderResponse | FailureResponse;
+export type CreateTransferOutput = SuccessfulTransferResponse | FailureResponse;
+
+export type GetOrderOutput = CreatePaymentLinkOutput;
+export type GetTransferOutput = CreateTransferOutput;
